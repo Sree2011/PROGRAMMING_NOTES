c44c49f24a73b734ea37e32a2f1603e9dd0a8d8e
Sree Sai Nandini Gundraju <gundrajusreesainandini@gmail.com>
2024-05-18 17:01:33 +0530

    added javaio

--- coding snippets/Basics/Input and output/inout.java
+++ coding snippets/Basics/Input and output/inout.java
@@ -1,46 +1,48 @@
 
-import java.util.*;
+import java.util.Scanner;
+
 public class inout {
 
     // Scanner class from java.util is used for input
     // println,printf and print from System.out are used for output
     public static void main(String[] args) {
-       Scanner sc = new Scanner(System.in);
-       // integer variable
-       int a = sc.nextInt();
-       System.out.println(a);
+        Scanner sc = new Scanner(System.in);
+        // integer variable
+        int a = sc.nextInt();
+        System.out.println(a);
 
-       // double variable
-       double d = sc.nextDouble();
-       System.out.println(d);
+        // double variable
+        double d = sc.nextDouble();
+        System.out.println(d);
 
-       // character variable
-       char c = sc.next().charAt(0);
-       System.out.println(c);
+        // character variable
+        char c = sc.next().charAt(0);
+        System.out.println(c);
 
-       // float variable
-       float f = sc.nextFloat();
-       System.out.println(f);
+        // float variable
+        float f = sc.nextFloat();
+        System.out.println(f);
 
-       // short variable
-       short s = sc.nextShort();
-       System.out.println(s);
+        // short variable
+        short s = sc.nextShort();
+        System.out.println(s);
 
+        // long variable
+        long l = sc.nextLong();
+        System.out.println(l);
 
-       // long variable
-       long l = sc.nextLong();
-       System.out.println(l);
+        // byte variable
+        byte by = sc.nextByte();
+        System.out.println(by);
 
-       // byte variable
-       byte b = sc.nextByte();
-       System.out.println(b);
+        // boolean variable
+        boolean b = sc.nextBoolean();
+        System.out.println(b);
 
-       // boolean variable
-       Boolean b = sc.nextBoolean();
-       System.out.println(b);
+        // string variable
+        String str = "hello";
+        System.out.println(str);
 
-       // string variable
-       String str = "hello";
-       System.out.println(str);
+        sc.close(); // close the scanner
     }
 }
